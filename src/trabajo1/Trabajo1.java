ecc663735005a50a6632f35ebade4fed1fe24ffc
SrKevenDC <kevin.damian16@hotmail.com>
2023-09-01 22:09:35 -0300

    prueba

--- src/trabajo1/Trabajo1.java
+++ src/trabajo1/Trabajo1.java
@@ -14,9 +14,9 @@ try{
 Automovil automovil1 = new Automovil(3,"Susuki",2009,50,50,60);
 automovil1.cargarEstanque(5);
 automovil1.vaciarEstanque(30);
-System.out.println("Debes pagar $" + automovil1.calcularImpuesto() + " de impuesto");
+System.out.println("Impuesto: " + automovil1.calcularImpuesto());
 automovil1.modificarAño(2010);
-System.out.println("El nuevo año del automovil es " + automovil1.getAño());
+System.out.println("Nuevo año: " + automovil1.getAño());
 automovil1.imprimirDatos();
 
 
@@ -26,10 +26,7 @@ System.out.println("ERROR: " + e.getMessage());
 }
 
 Motocicleta motocicleta1 = new Motocicleta(4,"kawasaki",2008,40,5,6);
-motocicleta1.cargarEstanque(10);
 motocicleta1.vaciarEstanque(9);
-System.out.println("Debes pagar $" + motocicleta1.calcularImpuesto() + " de impuesto");
-System.out.println("cilindrada: " + motocicleta1.getCilindrada());
 motocicleta1.imprimirDatos();
     }
     
